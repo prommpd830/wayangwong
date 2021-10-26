f51f45d1376ae35bc12b247cb21e9ed315166f52
andim830 <prommpd830@gmail.com>
2021-10-26 17:19:27 +0700

    update

--- js/script.js
+++ js/script.js
@@ -25,36 +25,41 @@ toggle.onclick = () => {
 	}
 }
 
+// Fixed Navbar
+$(window).scroll(function(){
+    $('.navbar').toggleClass('scrolling-active', $(this).scrollTop() > 0);
+});
 
-(function () {
-  "use strict";
 
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
+// (function () {
+//   "use strict";
 
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+//   // define variables
+//   var items = document.querySelectorAll(".timeline li");
 
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
-    }
-  }
+//   // check if an element is in viewport
+//   // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+//   // function isElementInViewport(el) {
+//   //   var rect = el.getBoundingClientRect();
+//   //   return (
+//   //     rect.top >= 0 &&
+//   //     rect.left >= 0 &&
+//   //     rect.bottom <=
+//   //       (window.innerHeight || document.documentElement.clientHeight) &&
+//   //     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   //   );
+//   // }
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-})();
+//   // function callbackFunc() {
+//   //   for (var i = 0; i < items.length; i++) {
+//   //     if (isElementInViewport(items[i])) {
+//   //       items[i].classList.add("in-view");
+//   //     }
+//   //   }
+//   // }
+
+//   // // listen for events
+//   // window.addEventListener("load", callbackFunc);
+//   // window.addEventListener("resize", callbackFunc);
+//   // window.addEventListener("scroll", callbackFunc);
+// })();
